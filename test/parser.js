7816a0f349349a29f69dd960933a0847f78eb95c
Dumitru Cantea <canteadumitru@gmail.com>
2016-08-17 23:11:35 +0300

    working words extractor

--- test/parser.js
+++ test/parser.js
@@ -4,7 +4,7 @@ var parser = require('../lib/parser');
 var assert = require('assert');
 
 var concepts;
-var text = 'fsg fgsdfg Moldova cum Partidul  Democrat dfsdfgd sfd hd hd\nPartidul Socialiștilor din R. Moldova (PSRM) îndeamnă cetățenii să organizeze proteste pașnice împotriva actualei guvernări. Anunțul a fost făcut astăzi, în cadrul unei conferințe de presă, de către liderul PSRM, Igor Dodon.\nDupă cum informează TRIBUNA, Igor Dodon a declarat că, R. Moldova se află într-o situație foarte dificilă.\n”Vin cu mesaj către cetățenii: haideți să nu tăcem, să nu temem, să protestăm împreună. Noi, socialiștii susținem orice protest împotriva guvernării, dar fără ideologii. Trebuie să luptăm cu actuala guvernare, trebuie să protestăm în continuarea, deoarece ei se tem de proteste”, a precizat el.\nTotodată, Dodon a menționat că, socialiștii insistă în continuare pentru a fi demiși: Guvernatorul, Dorin Drăguțanu, Procurorul General, Corneliu Gurin, dar și șeful Centrului Național Anticorupție (CNA), Viorel Chetraru.\nÎn același context, liderul PSRM a venit și cu un mesaj către cetățeni , ca în alegerile din 14 iunie curent, să nu voteze pentru candidații înaintați de partidele de la guvernare.\n”Pe data de 14 iunie nici un vot oligarhilor, așa ne va fi mai ușor să îi dăm jos de la guvernare. Trebuie să ne pregătim de alegeri anticipate, aceasta e unica soluție Corneliu Gurin”, a mai spus Dodon. Eurovision 2016.';
+var text = 'fsg fgsdfg Moldova cum Partidul \tDemocrat dfsdfgd sfd hd hd\nPartidul Socialiștilor din R. Moldova (PSRM) îndeamnă cetățenii să organizeze proteste pașnice împotriva actualei guvernări. Anunțul a fost făcut astăzi, în cadrul unei conferințe de presă, de către liderul PSRM, Igor Dodon.\nDupă cum informează TRIBUNA, Igor Dodon a declarat că, R. Moldova se află într-o situație foarte dificilă.\n”Vin cu mesaj către cetățenii: haideți să nu tăcem, să nu temem, să protestăm împreună. Noi, socialiștii susținem orice protest împotriva guvernării, dar fără ideologii. Trebuie să luptăm cu actuala guvernare, trebuie să protestăm în continuarea, deoarece ei se tem de proteste”, a precizat el.\nTotodată, Dodon a menționat că, socialiștii insistă în continuare pentru a fi demiși: Guvernatorul, Dorin Drăguțanu, Procurorul General, Corneliu Gurin, dar și șeful Centrului Național Anticorupție (CNA), Viorel Chetraru.\nÎn același context, liderul PSRM a venit și cu un mesaj către cetățeni , ca în alegerile din 14 iunie curent, să nu voteze pentru candidații înaintați de partidele de la guvernare.\n”Pe data de 14 iunie nici un vot oligarhilor, așa ne va fi mai ușor să îi dăm jos de la guvernare. Trebuie să ne pregătim de alegeri anticipate, aceasta e unica soluție Corneliu Gurin”, a mai spus Dodon. Eurovision 2016.';
 
 function doParse() {
 	return parser.parse({
@@ -23,13 +23,21 @@ describe('parser', function() {
 			country: 'md'
 		});
 		// console.log(concepts);
-		// assert.equal(3, concepts.length);
+		assert.equal(3, concepts.length);
 		assert.equal('Europa', concepts[0].value);
 		assert.equal('R. Moldova', concepts[1].value);
 		assert.equal('Europa', concepts[2].value);
 	});
 
-	return;
+	it('connect words', function() {
+		concepts = parser.parse({
+			text: 'Partidul Meu din R. Moldova. Nu exista.',
+			lang: 'ro',
+			country: 'md'
+		});
+		// console.log(concepts);
+		assert.equal(1, concepts.length);
+	});
 
 	it('parse', function() {
 		concepts = parser.parse({
@@ -37,10 +45,11 @@ describe('parser', function() {
 			lang: 'ro',
 			country: 'md'
 		});
+		// console.log('concepts', concepts);
 		assert.equal(20, concepts.length);
 		assert.equal('Republica Moldova', concepts[0].name);
-		// console.log('concepts', concepts);
 	});
+	// return;
 	it('concepts split', function() {
 		concepts.forEach(function(concept) {
 			var list = concept.split();
@@ -57,6 +66,7 @@ describe('parser', function() {
 		}, {
 			mode: 'collect'
 		});
+		// console.log('mode', concepts);
 		assert.equal(14, concepts.length);
 	});
 	it('no normalizeText', function() {
