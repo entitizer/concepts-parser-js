7903e1b767d2cdc9ae7abec88d1c70472fc3d27e
Dumitru Cantea <canteadumitru@gmail.com>
2017-04-18 10:27:38 +0300

    remove entitizer.core

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,38 @@
-'use strict';
 
-import { utils } from 'entitizer.core';
+const Atonic = require('atonic');
+// import * as crypto from 'crypto';
 
-export const isDigit = utils.isDigit;
-export const isLower = utils.isLower;
-export const isUpper = utils.isUpper;
-export const isLetter = utils.isLetter;
-export const isLetterOrDigit = utils.isLetterOrDigit;
+export function atonic(s: string): string {
+	return Atonic(s);
+}
+
+// export function sha1(value: string): string {
+// 	return crypto.createHash('sha1').update(value, 'utf8').digest('hex').toLowerCase();
+// }
+
+// export function md5(value: string): string {
+// 	return crypto.createHash('md5').update(value, 'utf8').digest('hex').toLowerCase();
+// }
+
+export function isLetter(s: string): boolean {
+	return s.toUpperCase() !== s.toLowerCase();
+}
+
+export function isUpper(s: string): boolean {
+	return isLetter(s) && s.toUpperCase() === s;
+}
+
+export function isLower(s: string): boolean {
+	return isLetter(s) && s === s.toLowerCase();
+}
+
+export function isDigit(s: string): boolean {
+	return /^\d+$/.test(s);
+}
+
+export function isLetterOrDigit(s: string): boolean {
+	return isDigit(s) || isLetter(s);
+}
 
 export function isPunctuation(s: string): boolean {
 	return /[!"#%&'\(\)\*,\.\/:\?@\[\]\\_{}-]/.test(s);
