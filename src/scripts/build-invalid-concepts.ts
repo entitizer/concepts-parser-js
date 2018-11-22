7b8f85e0f8152d7d1a9f3f922ce2944585d5b00e
Mitica <canteadumitru@gmail.com>
2018-11-22 13:55:01 +0200

    added valid_concepts.txt
    for errors in stopwords module

--- src/scripts/build-invalid-concepts.ts
+++ src/scripts/build-invalid-concepts.ts
@@ -7,13 +7,40 @@ const fs = require('fs');
 const join = require('path').join;
 
 for (let lang of getLanguages()) {
+    let invalidConcepts = getFileInvalidConcepts(lang);
+    invalidConcepts = invalidConcepts.concat(stopwords[lang]);
+    invalidConcepts = normalizeConcepts(invalidConcepts);
+    const validConcepts = normalizeConcepts(getFileValidConcepts(lang));
+    invalidConcepts = invalidConcepts.filter(item => validConcepts.indexOf(item) < 0);
+    saveFileInvalidConcepts(lang, invalidConcepts.join('\n'));
+}
+
+function normalizeConcepts(concepts: string[]) {
+    concepts = concepts.filter(item => !!item && item.trim().length);
+    concepts = concepts.map(item => atonic.lowerCase(item.toLowerCase().trim()).replace(/\[\\s\]/, '[\\S]'));
+    // uniq
+    concepts = uniq(concepts);
+    concepts = concepts.sort();
+
+    return concepts;
+}
+
+function saveFileInvalidConcepts(lang: string, data: string) {
+    const file = join(__dirname, '..', '..', 'data', lang, 'invalid_concepts.txt');
+    fs.writeFileSync(file, data, 'utf8');
+}
+
+function getFileInvalidConcepts(lang: string) {
     const file = join(__dirname, '..', '..', 'data', lang, 'invalid_concepts.txt');
     let invalidConcepts: string[] = fs.readFileSync(file, 'utf8').split(/\n+/g);
-    invalidConcepts = invalidConcepts.concat(stopwords[lang]);
-    invalidConcepts = invalidConcepts.filter(item => !!item && item.trim().length);
-    invalidConcepts = invalidConcepts.map(item => atonic.lowerCase(item.toLowerCase().trim()));
-    // uniq
-    invalidConcepts = uniq(invalidConcepts);
-    invalidConcepts = invalidConcepts.sort();
-    fs.writeFileSync(file, invalidConcepts.join('\n'), 'utf8');
+    return invalidConcepts;
+}
+
+function getFileValidConcepts(lang: string) {
+    const file = join(__dirname, '..', '..', 'data', lang, 'valid_concepts.txt');
+    try {
+        return fs.readFileSync(file, 'utf8').split(/\n+/g) as string[];
+    } catch (e) {
+        return [];
+    }
 }
