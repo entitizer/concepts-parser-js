7bc6749638ee10b3e6108ff243852fd81cccddcf
Mitica <canteadumitru@gmail.com>
2019-01-02 06:33:36 +0200

    fix isAbbrOf

--- src/filters/abbr.ts
+++ src/filters/abbr.ts
@@ -2,31 +2,7 @@
 const debug = require('debug')('concepts:filter');
 import { Context } from '../types';
 import { Concept } from '../concept';
-
-function isAbbrOf(text: string, abbr: string): boolean {
-	const words: string[] = text.split(/[ ]+/g);
-	let position: number = 0;
-
-	for (let i = 0; i < abbr.length; i++ , position++) {
-		if (position === words.length) {
-			return false;
-		}
-		let word = words[position];
-		if (word[0].toUpperCase() !== abbr[i]) {
-			if (word.length > 6 || position === words.length - 1) {
-				return false;
-			}
-
-			position++;
-			word = words[position];
-			if (word[0].toUpperCase() !== abbr[i]) {
-				return false;
-			}
-		}
-	}
-	debug(abbr, 'is abbr for: `', text, '`');
-	return true;
-}
+import isAbbrOf = require('is-abbr-of');
 
 function isInParentheses(concept: Concept, context: Context): boolean {
 	var i = concept.index;
@@ -45,7 +21,8 @@ export function filter(concepts: Concept[], context: Context): Concept[] {
 	return concepts.filter(function (concept) {
 		if (prev && concept.isAbbr && prev.endIndex < concept.index && isInParentheses(concept, context)) {
 			const text = context.text.substring(prev.index, concept.index - 2);
-			if (isAbbrOf(text, concept.value)) {
+			if (isAbbrOf(concept.value, text)) {
+				debug(`${concept.value} is abbr of ${text}`);
 				prev.abbr = concept.value;
 				prev.reset(text, 0, context.lang);
 				// return false;
